4791e55788254b0523bd5119dedd978a35f28dfd
1034948 <1034948@student.sae.edu.au>
2025-11-08 19:17:52 +1100

    Scripted bread butterer microgame
    Made game scripts when pressing button, bread gets buttered, imported sprites, set up tilemap painting

---
+++ Assets/Bread Butterer/Scripts/ButteringBread.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ButteringBread : MonoBehaviour
+{
+
+    public int timesButtered = 0;
+
+    [SerializeField] GameObject ButteredSectionBread;
+    [SerializeField] GameObject ButteredSectionBread1;
+    [SerializeField] GameObject ButteredSectionBread2;
+
+    public Tile butteredBread;
+
+    public Vector3Int position;
+
+    public Tilemap tilemap;
+    [ContextMenu("Paint")]
+
+    void Start()
+    {
+        ButteredSectionBread.SetActive(false);
+        ButteredSectionBread1.SetActive(false);
+        ButteredSectionBread2.SetActive(false);
+    }
+
+    public void ButtonPressed()
+    {
+        timesButtered += 1;
+
+        if (timesButtered == 1)
+        {
+            ButteredSectionBread.SetActive(true);
+        }
+
+        if (timesButtered == 3)
+        {
+            ButteredSectionBread1.SetActive(true);
+        }
+
+        if (timesButtered == 4)
+        {
+            ButteredSectionBread2.SetActive(true);
+        }
+
+        if (timesButtered == 5)
+        {
+            ButteredSectionBread.SetActive(false);
+            ButteredSectionBread1.SetActive(false);
+            ButteredSectionBread2.SetActive(false);
+
+            Paint();
+        }
+    }
+
+    public void Paint()
+    {
+        tilemap.SetTile(position, butteredBread);
+    }
+
+}
